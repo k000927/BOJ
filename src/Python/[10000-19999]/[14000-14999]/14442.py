d94b47befcf74a4cb37917f6a37063b389172264
nnijgnus <kimsungjin927@gmail.com>
2024-03-09 13:56:08 +0900

    14442 문제풀이

---
+++ src/Python/[10000-19999]/[14000-14999]/14442.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+
+n, m, k = map(int, sys.stdin.readline().split())
+graph = [list(sys.stdin.readline()) for _ in range(n)]
+wallCnt = [[1000001 for _ in range(m)] for _ in range(n)]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+queue = deque()
+queue.append((0, 0, 0, 1))
+
+while queue:
+    i, j, wall, cnt = queue.popleft()
+    if i == n - 1 and j == m - 1:
+        print(cnt)
+        exit()
+    for a in range(4):
+        x = i + dx[a]
+        y = j + dy[a]
+        if 0 <= x < n and 0 <= y < m:
+            if graph[x][y] == "1" and wall < k and wallCnt[x][y] > wall + 1:
+                wallCnt[x][y] = wall + 1
+                queue.append((x, y, wall + 1, cnt + 1))
+            elif graph[x][y] == "0" and wallCnt[x][y] > wall:
+                wallCnt[x][y] = wall
+                queue.append((x, y, wall, cnt + 1))
+
+print(-1)

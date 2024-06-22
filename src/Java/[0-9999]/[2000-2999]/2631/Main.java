81365f0f35985759ed45bee5c2e928c074567fa3
k000927 <kimsungjin927@gmail.com>
2024-06-22 14:18:27 +0900

    2631 문제풀이

---
+++ src/Java/[0-9999]/[2000-2999]/2631/Main.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] kids = new int[n];
+        for (int i = 0; i < n; i++) {
+            kids[i] = Integer.parseInt(br.readLine());
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int ans = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (kids[i] > kids[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            if (dp[i] == 0) dp[i] = 1;
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(n - ans);
+    }
+}

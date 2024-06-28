3a2a4d3d5462804583f9c51a45950a3ccf5837f4
k000927 <kimsungjin927@gmail.com>
2024-06-28 15:43:23 +0900

    6443 문제풀이

---
+++ src/Java/[0-9999]/[6000-6999]/6443/Main.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] alphacnt;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        sb = new StringBuilder();
+        String input;
+        alphacnt = new int[26];
+
+        for (int i = 0; i < N; ++i) {
+            input = br.readLine().toString();
+
+            for (int j = 0; j < input.length(); j++) {
+                alphacnt[input.charAt(j) - 'a']++;
+            }
+
+            dfs(input.length(), sb);
+
+            for (int j = 0; j < 26; j++) {
+                alphacnt[j] = 0;
+            }
+        }
+
+    }
+
+    public static void dfs(int maxlen, StringBuilder sb) {
+
+        if (maxlen == sb.length()) {
+            System.out.println(sb.toString());
+            return;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (alphacnt[i] > 0) {
+                alphacnt[i]--;
+                sb.append((char) (i + 'a'));
+                dfs(maxlen, sb);
+                alphacnt[i]++;
+                sb.setLength(sb.length() - 1);
+            }
+        }
+    }
+}

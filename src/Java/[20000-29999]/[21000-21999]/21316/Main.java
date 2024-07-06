2b53460716dee610b0c8cc9d1005816ea9ccaf71
k000927 <kimsungjin927@gmail.com>
2024-07-06 15:34:34 +0900

    21316 문제풀이

---
+++ src/Java/[20000-29999]/[21000-21999]/21316/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int[] numEdges = new int[13];
+        ArrayList<Integer>[] graph = new ArrayList[13];
+        for (int i = 1; i < 13; i++) graph[i] = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 12; i++) {
+            StringTokenizer line = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(line.nextToken());
+            int b = Integer.parseInt(line.nextToken());
+            numEdges[a]++;
+            numEdges[b]++;
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        int spica = -1;
+        for (int i = 1; i <= 12; i++) {
+            if (numEdges[i] != 3) continue;
+
+            int sum = 0;
+            for (int adj : graph[i]) sum += numEdges[adj];
+            if (sum == 6) {
+                spica = i;
+                break;
+            }
+        }
+
+        System.out.println(spica);
+    }
+}

5a8f399aed329360467182a204d0e96e0c6b7967
k000927 <kimsungjin927@gmail.com>
2025-02-10 23:48:04 +0900

    1402 문제풀이

---
+++ src/Java/[0-9999]/[1000-1999]/1402/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int t;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            System.out.println("yes");
+        }
+    }
+}

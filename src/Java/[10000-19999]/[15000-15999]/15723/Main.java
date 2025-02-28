4e55aaf31956703dfc745c0bc436619c6752d56d
k000927 <kimsungjin927@gmail.com>
2025-02-28 20:53:25 +0900

    3613 문제풀이

--- src/Java/[10000-19999]/[15000-15999]/15723/Main.java
+++ src/Java/[10000-19999]/[15000-15999]/15723/Main.java
@@ -1,54 +1,54 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        HashMap<String, String> map = new HashMap<>();
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-            String start = st.nextToken();
-            st.nextToken();
-            String end = st.nextToken();
+    HashMap<String, String> map = new HashMap<>();
+    int n = Integer.parseInt(br.readLine());
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
 
-            map.put(start, end);
-        }
+      String start = st.nextToken();
+      st.nextToken();
+      String end = st.nextToken();
 
-        Boolean[] visited = new Boolean[26];
-        int m = Integer.parseInt(br.readLine());
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(visited, false);
-            st = new StringTokenizer(br.readLine());
-
-            String start = st.nextToken();
-            st.nextToken();
-            String end = st.nextToken();
-
-            while (true) {
-                String curNode = map.getOrDefault(start, "NOT_EXIST");
-
-                if (curNode.equals("NOT_EXIST")) {
-                    System.out.println("F");
-                    break;
-                }
-
-                if (visited[(int) curNode.charAt(0) - 'a']) {
-                    System.out.println("F");
-                    break;
-                }
-
-                if (curNode.equals(end)) {
-                    System.out.println("T");
-                    break;
-                }
-
-                start = curNode;
-            }
-        }
+      map.put(start, end);
     }
+
+    Boolean[] visited = new Boolean[26];
+    int m = Integer.parseInt(br.readLine());
+    for (int i = 0; i < m; i++) {
+      Arrays.fill(visited, false);
+      st = new StringTokenizer(br.readLine());
+
+      String start = st.nextToken();
+      st.nextToken();
+      String end = st.nextToken();
+
+      while (true) {
+        String curNode = map.getOrDefault(start, "NOT_EXIST");
+
+        if (curNode.equals("NOT_EXIST")) {
+          System.out.println("F");
+          break;
+        }
+
+        if (visited[(int) curNode.charAt(0) - 'a']) {
+          System.out.println("F");
+          break;
+        }
+
+        if (curNode.equals(end)) {
+          System.out.println("T");
+          break;
+        }
+
+        start = curNode;
+      }
+    }
+  }
 }

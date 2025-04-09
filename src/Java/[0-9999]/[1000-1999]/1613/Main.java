5f145c8ec8a6d9ff057afb6bcc987b35bce8179d
k000927 <kimsungjin927@gmail.com>
2025-04-09 23:06:54 +0900

    1613 문제풀이

--- src/Java/[0-9999]/[1000-1999]/1613/Main.java
+++ src/Java/[0-9999]/[1000-1999]/1613/Main.java
@@ -1,67 +1,62 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.StreamTokenizer;
+
+/*
+ * 플로이드 워셜을 이용해 인접 리스트를 작성한다.
+ *
+ * A -> B가 가능하다면 A가 먼저 발생한 사건이다.
+ * A -> B가 불가능하다면
+ * 		B -> A가 가능하다면 B가 먼저 발생한 사건이다.
+ * 		B -> A가 불가능하다면 두 사건은 연관이 없다.
+ */
 
 public class Main {
+    static final StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
 
     public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder ans = new StringBuilder();
+        int N, K, S;
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        N = nextInt();
+        K = nextInt();
 
-        boolean[][] arr = new boolean[N + 1][N + 1];
+        boolean[][] adj = new boolean[401][401];
+
         for (int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            arr[x][y] = true;
+            adj[nextInt()][nextInt()] = true;
         }
 
-        // 플로이드 와샬 알고리즘
         for (int k = 1; k <= N; k++) {
             for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (arr[i][k] && arr[k][j]) {
-                        arr[i][j] = true;
-                    }
+                if (adj[i][k]) {
+                    for (int j = 1; j <= N; j++)
+                        adj[i][j] = adj[i][j] || (adj[i][k] && adj[k][j]);
                 }
             }
         }
 
-        int S = Integer.parseInt(br.readLine());
+        S = nextInt();
+        int A, B;
+        for (int i = 0; i < S; i++) {
+            A = nextInt();
+            B = nextInt();
 
-        StringBuilder sb = new StringBuilder();
-        while (S-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            if (adj[A][B])
+                ans.append(-1).append('\n');
+            else if (adj[B][A])
+                ans.append(1).append('\n');
+            else
+                ans.append(0).append('\n');
 
-            if (x < 1 || y < 1 || x > N || y > N) { // 주어진 범위에서 벗어날 경우
-                sb.append("0\n");
-            } else {
-                if (arr[x][y]) { // x에서 y로 갈 수 있다는 뜻은 사건이 먼저 일어났다는 뜻
-                    sb.append("-1\n");
-                } else {
-                    if (arr[y][x]) { // y에서 x로 갈 수 있다는 뜻은 사건이 나중에 일어났다는 뜻
-                        sb.append("1\n");
-                    } else { // x에서 y, y에서 x로 모두 갈 수 없으면 상관 없는 사건 관계임
-                        sb.append("0\n");
-                    }
-                }
-            }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(ans);
     }
 
+    private static int nextInt() throws IOException {
+        st.nextToken();
+        return (int) st.nval;
+    }
 }

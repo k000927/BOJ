3e9fdb3d68da9d321884bf3f947391f5659c12d0
k000927 <kimsungjin927@gmail.com>
2025-06-22 01:08:18 +0900

    2812 문제풀이

---
+++ src/Java/[0-9999]/[2000-2999]/2812/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        char[] arr = br.readLine().toCharArray();
+        Deque<Character> dq = new ArrayDeque<>();
+        for (int i = 0; i < N; i++) {
+            while (K > 0 && !dq.isEmpty() && dq.getLast() < arr[i]) {
+                dq.removeLast();
+                K--;
+            }
+            dq.addLast(arr[i]);
+        }
+
+        StringBuilder ans = new StringBuilder();
+        while (dq.size() > K) {
+            ans.append(dq.removeFirst());
+        }
+
+        System.out.println(ans);
+    }
+
+}

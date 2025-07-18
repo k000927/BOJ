b9fa09ee10e0631f3cbc300f8931e870dfd07f0e
k000927 <kimsungjin927@gmail.com>
2025-07-19 00:38:27 +0900

    1013 문제풀이

--- src/Java/[0-9999]/[1000-1999]/1013/Main.java
+++ src/Java/[0-9999]/[1000-1999]/1013/Main.java
@@ -1,52 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder ans = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+
+        String regex = "(100+1+|01)+";
+
         int T = Integer.parseInt(br.readLine());
-
         while (T-- > 0) {
-            char[] code = br.readLine().toCharArray();
-
-            int index = 0;
-            while (true) {
-                try {
-                    if (index == code.length) {
-                        ans.append("YES").append('\n');
-                        break;
-                    } else if (code[index] == '1' && code[index + 1] == '0') {
-                        index += 2;
-                        if (code[index] != '0') {
-                            ans.append("NO").append('\n');
-                            break;
-                        }
-                        while (code[index] == '0') {
-                            index++;
-                        }
-
-                        if (code[index] != '1') {
-                            ans.append("NO").append('\n');
-                            break;
-                        }
-                        while (index < code.length && code[index] == '1') {
-                            index++;
-                        }
-                    } else if (code[index] == '0' && code[index + 1] == '1') {
-                        index += 2;
-                    } else {
-                        ans.append("NO").append('\n');
-                        break;
-                    }
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    ans.append("NO").append('\n');
-                    break;
-                }
-            }
+            if(br.readLine().matches(regex))
+                sb.append('Y').append('E').append('S').append('\n');
+            else
+                sb.append('N').append('O').append('\n');
         }
 
-        System.out.print(ans);
+        System.out.println(sb);
     }
 }

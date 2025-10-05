862633a87dcff089a559f11a866b241e7233a933
k000927 <kimsungjin927@gmail.com>
2025-10-05 18:55:56 +0900

    11689 문제풀이

---
+++ src/Java/[10000-19999]/[11000-11999]/11689/Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    static long N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Long.parseLong(br.readLine());
+
+        long res = N;
+        for (long i = 2; i < Math.sqrt(N); i++) {
+            if (N % i == 0) {
+                res -= res / i;
+                while (N % i == 0) N /= i;
+            }
+        }
+
+        if (N > 1) res -= res / N;
+        System.out.println(res);
+    }
+}

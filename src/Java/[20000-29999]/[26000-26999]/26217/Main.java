034f264c7688f0813a6f8558b955daa40747f7f3
k000927 <kimsungjin927@gmail.com>
2026-03-16 00:34:40 +0900

    26217 문제풀이

---
+++ src/Java/[20000-29999]/[26000-26999]/26217/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        double ans = 0;
+        for (int i = 0; i < N; i++) {
+            ans += (double) N / (N-i);
+        }
+
+        System.out.println(ans);
+    }
+}
